363333db49e977e66ccb0e1adea998c1fa16fe15
Abhinav Dwivedi <learnerabhinavdwivedi@gmail.com>
2026-02-23 18:06:10 +0530

    app

--- devtalkx-frontend/src/utils/constants.js
+++ devtalkx-frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
 // 🚀 This is the address of your Backend "Power Plant"
-export const BASE_URL = "http://localhost:5002";
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5002";
